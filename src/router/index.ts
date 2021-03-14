4d297a6858d542e4377e83c3366a20428d66279e
Wilcymar Tavares <wilcymar.silva@gmail.com>
2021-03-14 20:53:05 -0300

    atualizacoes apos a API

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
+import MyCards from '../views/MyCards.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/mycards',
+    name: 'MyCards',
+    component: MyCards,
   },
   {
     path: '/about',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <input v-model="a" type="text">
+    <div v-for="(poke, index) in data.pokemons" :key="poke.url">
+      <h1>{{index}} - {{poke.name}}</h1>
+    </div>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, ref, reactive } from 'vue';
+import axios from 'axios';
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  setup() {
+    const a = ref();
+    const data = reactive({
+      pokemons: [],
+    });
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then((res) => {
+      console.log(res);
+      data.pokemons = res.data.results;
+    });
+
+    return { a, data };
   },
+  // name: 'Home',
+  // components: {
+  //   HelloWorld,
+  // },
 });
 </script>
